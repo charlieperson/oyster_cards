788b5e5c2ee9f7d05534dfdde1eb406625b82aef
hibreez <4ngus.whitehead@gmail.com>
2015-11-30 16:26:56 +0000

    completed 2nd user story - topup

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,15 +1,22 @@
 describe "User Stories" do
+  let(:card) { Oystercard.new }
 
   # In order to use public transport
   # As a customer
   # I want money on my card
   it 'card has a balance' do
-    card = Oystercard.new
     expect(card).to respond_to(:balance)
   end
 
   it 'card is initialized with a balance of 0' do
-    card = Oystercard.new
     expect(card.balance).to eq 0
+  end
+
+  #In order to keep using public transport
+  #As a customer
+  #I want to add money to my card
+  it 'can add money to card' do
+    card.top_up! 10
+    expect(card.balance).to eq 10
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,13 +3,20 @@ require "oystercard"
 describe Oystercard do
   subject(:card) { described_class.new }
 
-  it "has a balance" do
-    expect(card).to respond_to(:balance)
+  describe '#balance' do
+    it "has a balance" do
+      expect(card).to respond_to(:balance)
+    end
+
+    it 'is initialized with balance of 0' do
+      expect(card.balance).to eq 0
+    end
   end
 
-  it 'is initialized with balance of 0' do
-    card = Oystercard.new
-    expect(card.balance).to eq 0
+  describe '#top_up!' do
+    it 'can be topped up' do
+      card.top_up! 10
+      expect(card.balance).to eq 10
+    end
   end
-
 end
