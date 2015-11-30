74b48aee6ffe1bb0cca2cb38b268efdab4380a19
hibreez <4ngus.whitehead@gmail.com>
2015-11-30 16:03:00 +0000

    completed first user story - balance

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -3,9 +3,13 @@ describe "User Stories" do
   # In order to use public transport
   # As a customer
   # I want money on my card
-  it 'card has money' do
+  it 'card has a balance' do
     card = Oystercard.new
     expect(card).to respond_to(:balance)
   end
 
+  it 'card is initialized with a balance of 0' do
+    card = Oystercard.new
+    expect(card.balance).to eq 0
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,4 +7,9 @@ describe Oystercard do
     expect(card).to respond_to(:balance)
   end
 
+  it 'is initialized with balance of 0' do
+    card = Oystercard.new
+    expect(card.balance).to eq 0
+  end
+
 end
